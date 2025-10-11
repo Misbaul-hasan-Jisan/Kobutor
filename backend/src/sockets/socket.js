c7c467d0731413ecc2e4708dae6c614c6c00bc5c
Misbaul-hasan-jisan <jisanaa22@gmail.com>
2025-10-11 17:53:57 +0600

    location fixed

--- backend/src/sockets/socket.js
+++ backend/src/sockets/socket.js
@@ -9,11 +9,7 @@ const chatPageUsers = new Map(); // Track users specifically on chat page
 export const initIO = (server) => {
   io = new Server(server, {
     cors: {
-      origin: [
-        "http://localhost:5173",
-        "http://localhost:5174",
-        "http://localhost:5175",
-      ],
+      origin: "*",
       methods: ["GET", "POST"],
     },
   });
